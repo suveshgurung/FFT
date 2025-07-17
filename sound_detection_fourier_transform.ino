1e8fa0cbfe2c02e4f10a445f77be9b6136a54f4e
suvesh <suveshgurung2@gmail.com>
2025-07-17 09:44:49 +0545

    microphone now detects negative amplitudes as well

--- sound_detection_fourier_transform.ino
+++ sound_detection_fourier_transform.ino
@@ -1,8 +1,7 @@
 #define READ_FROM_SOUND_SENSOR_FLAG 22
 #define SOUND_SENSOR_ANALOG_INPUT 4
 
-#define SAMPLING_FREQUENCY 500
-#define SAMPLE_TIME 2000        // 2500 microseconds sampling time
+#define SAMPLE_TIME 250        // 250 microseconds sampling time
 
 #define ADC_MAX_VALUE 4095.0
 #define ADC_MAX_VOLT 3.3
@@ -11,7 +10,6 @@
 
 unsigned long current_time = 0, previous_time = 0, elasped_time;
 unsigned long current_timeout_time = 0, previous_timeout_time = 0, elasped_timeout_time;
-unsigned long number_of_samples = 0;
 float calibrated_base_value = 0.0;
 int is_first_data = 1;
 
@@ -48,30 +46,25 @@ void loop() {
     if (elasped_time > SAMPLE_TIME) {
       float analog_value = analogRead(SOUND_SENSOR_ANALOG_INPUT);
       float analog_equivalent_voltage = (ADC_MAX_VOLT / ADC_MAX_VALUE) * analog_value;
-      analog_value = ADC_MAX_VALUE;     // Reset the analog_value to max.
 
-      if (analog_equivalent_voltage < calibrated_base_value) {
+      if (is_first_data) {
+        Serial.println("%");        // Delimeter character to signify first data is coming.
+        is_first_data = 0;
         previous_timeout_time = millis();
-        if (is_first_data) {
-          Serial.println("%");        // Delimeter character to signify first data is coming.
-          is_first_data = 0;
-        }
+      }
 
-        number_of_samples++;
-        analog_equivalent_voltage = calibrated_base_value - analog_equivalent_voltage;
-        Serial.println(analog_equivalent_voltage);
-        analog_equivalent_voltage = calibrated_base_value;
-      }
-      else if (!is_first_data) {
-        current_timeout_time = millis();
-        elasped_timeout_time = current_timeout_time - previous_timeout_time;
-      }
+      float ac_signal = analog_equivalent_voltage - calibrated_base_value;
+      Serial.println(ac_signal);
+
       previous_time = current_time;
+      previous_timeout_time = millis();
     }
   }
 
   // Send a data end flag.
   if (!is_first_data) {
+    current_timeout_time = millis();
+    elasped_timeout_time = current_timeout_time - previous_timeout_time;
     if (elasped_timeout_time > TIMEOUT) {
       Serial.println("%e");
       elasped_timeout_time = 0;
